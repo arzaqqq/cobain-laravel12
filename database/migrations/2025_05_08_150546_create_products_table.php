496533edc1c5d58e4cc45ccaf090371cc630bd5d
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-05-08 22:54:10 +0700

    ADD : Membuat 2 tabel database produk dan kategori

---
+++ database/migrations/2025_05_08_150546_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id_produk');
+            $table->string('naa_product',150);
+            $table->integer('harge');
+
+            $table->integer('kategory_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
